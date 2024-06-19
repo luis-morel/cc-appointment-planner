9904e519a571d2037401cae1bc1e5e4c0ad9ba08
Luis Morel <luis.morel@gmail.com>
2024-06-19 15:08:29 -0400

    Updates to Tile

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ name, description }) => {
+  const values = Object.values(description);
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {values.map((value, i) => {
+        return (
+          <p key={i} className="tile">{value}</p>
+        )
+      })}
     </div>
   );
 };
