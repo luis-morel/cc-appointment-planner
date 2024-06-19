98dce52ceda4a9192763f5ee4f448a021792829b
Luis Morel <luis.morel@gmail.com>
2024-06-19 14:09:53 -0400

    Corrections to ContactForm

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -16,20 +16,20 @@ export const ContactForm = ({
         type="text"
         value={name}
         placeholder="Name"
-        onChange={setName}
+        onChange={(e) => setName(prevName => e.target.value)}
       />
       <input 
         type="email"
         value={email}
         placeholder="Email"
-        onChange={setEmail}
+        onChange={(e) => setEmail(prevEmail => e.target.value)}
       />
       <input 
         type="tel"
         value={phone}
         placeholder="Phone"
         pattern="/[1-9]-[0-9]{3}-[0-9]{3}-[0-9]{4}"
-        onChange={setPhone}
+        onChange={(e) => setPhone(prevPhone => e.target.value)}
       />
     </form>
   );
