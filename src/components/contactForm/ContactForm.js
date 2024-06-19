cf34c4310ae85075125d55a2914054bdde7f9797
Luis Morel <luis.morel@gmail.com>
2024-06-19 13:43:07 -0400

    Updates to ContactForm

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -9,8 +9,29 @@ export const ContactForm = ({
   setEmail,
   handleSubmit
 }) => {
+
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        value={name}
+        placeholder="Name"
+        onChange={setName}
+      />
+      <input 
+        type="email"
+        value={email}
+        placeholder="Email"
+        onChange={setEmail}
+      />
+      <input 
+        type="tel"
+        value={phone}
+        placeholder="Phone"
+        pattern="/[1-9]-[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        onChange={setPhone}
+      />
+    </form>
   );
 };
 
